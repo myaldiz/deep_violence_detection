4ffe7c7c7f2d00e6aeb3c4b63e14a2e5dc06afb5
Mustafa Berk Yaldiz <berk17@gmail.com>
2019-04-11 14:00:41 -0400

    preprocess implemented
    Need to time it with threaded settings

--- ViolanceDetection-Python/preprocess.py
+++ ViolanceDetection-Python/preprocess.py
@@ -1,6 +1,7 @@
 import random, time, ffmpeg
 import numpy as np
 
+
 # Reads train/test filenames from provided splits
 # Returns video directions and their labels in a list
 def get_data_dir(filename):
@@ -39,52 +40,40 @@ def read_clip(dirname, model_settings):
     video_width = int(video_info["width"])
     video_height = int(video_info["height"])
 
+    x_pos = max(video_width - video_height, 0) // 2
+    y_pos = max(video_height - video_width, 0) // 2
+    crop_size1 = min(video_height, video_width)
+
     # Input video
     ff = ffmpeg.input(dirname)
     # Divide into frames
     ff = ffmpeg.filter(ff, 'fps', fps)
     # Crop
-    x_pos = max(video_width - video_height, 0) // 2
-    y_pos = max(video_height - video_width, 0) // 2
-    crop_size1 = min(video_height, video_width)
     ff = ffmpeg.crop(ff, x_pos, y_pos, crop_size1, crop_size1)
     # Subsample
     ff = ffmpeg.filter(ff, 'scale', crop_size, crop_size)
+    # Horizontal flip with some probability
     if horizontal_flip > 0.5:
         ff = ffmpeg.hflip(ff)
+
     ff = ffmpeg.output(ff, 'pipe:',
                        format='rawvideo',
                        pix_fmt='rgb24')
-    out, err = ff.run(capture_stdout=True)
-    video = np.frombuffer(out, np.uint8).\
+    out, _ = ffmpeg.run(ff, capture_stdout=True, quiet=True)
+    video = np.frombuffer(out, np.uint8). \
         reshape([-1, crop_size, crop_size, 3])
-    video = video - np_mean
 
+    start_index = random.randint(0, video.shape[0] - fps)
+    video = video[start_index:start_index + fps]
+    # Substracts the mean and converts type to float32
+    video = video - np_mean
     return video
 
 
-
-    # # TODO: Check and correct this and following part
-    # if (len(tmp_data) == 0):
-    #     return np.array([])
-    # img_datas = []
-    #
-    # for j in range(len(tmp_data)):
-    #     img = Image.fromarray(tmp_data[j].astype(np.uint8))
-    #     if (img.width > img.height):
-    #         scale = float(crop_size) / float(img.height)
-    #         img = np.array(cv2.resize(np.array(img),
-    #                                   (int(img.width * scale + 1),
-    #                                    crop_size))).astype(np.float32)
-    #     else:
-    #         scale = float(crop_size) / float(img.width)
-    #         img = np.array(cv2.resize(np.array(img),
-    #                                   (crop_size,
-    #                                    int(img.height * scale + 1)))).astype(np.float32)
-    #     crop_x = int((img.shape[0] - crop_size) / 2)
-    #     crop_y = int((img.shape[1] - crop_size) / 2)
-    #     img = img[crop_x:crop_x + crop_size, crop_y:crop_y + crop_size, :] - np_mean[j]
-    #
-    #
-    #     img_datas.append(img)
-    # return np.array(img_datas).astype(np.float32)
+# TODO: Check reading with thread and time it consumes
+#time0 = time.time()
+#for read_index in range(10):
+#    video_dir, label = dir_videos[read_index], label_clips[read_index]
+#    video_dir = model_settings['data_home'] + video_dir
+#    video = read_clip(video_dir, model_settings)
+#print('Time diff:', time.time() - time0)

a0faa0c58f2d025ee9b2790f0f92a96d63aabe9f
Mustafa Berk Yaldiz <berk17@gmail.com>
2019-04-21 15:07:59 -0400

    data setup

--- datasets/HockeyFights-Splits/generate_txt.py
+++ datasets/HockeyFights-Splits/generate_txt.py
@@ -3,29 +3,19 @@ pwd = sys.path[0]+'/'
 import os
 import fnmatch
 
-test_split = 0.3
-
-file1 = open(pwd+'testlist.txt','w')
-file2 = open(pwd+'trainlist.txt','w')
+file = open(pwd+'datalist.txt','w')
 
 names = os.listdir(pwd+'../HockeyFights')
 
 class1 = fnmatch.filter(names,'no*')
-k = int(len(class1)*test_split)
-for name in class1[:k]:
-	file1.write(name+' 1\n')
-for name in class1[k:]:
-	file2.write(name+' 1\n')
+for name in class1:
+	file.write(name+' 1\n')
 
 class2 = fnmatch.filter(names,'fi*')
-k = int(len(class2)*test_split)
-for name in class2[:k]:
-	file1.write(name+' 2\n')
-for name in class2[k:]:
-	file2.write(name+' 2\n')
+for name in class2:
+	file.write(name+' 2\n')
 
-file1.close()
-file2.close()
+file.close()
 
 with open(pwd+'classInd.txt','w') as file:
 	file.write('1 NonViolence\n2 Violence')
--- datasets/Peliculas-Splits/generate_txt.py
+++ datasets/Peliculas-Splits/generate_txt.py
@@ -2,27 +2,17 @@ import sys
 pwd = sys.path[0]+'/'
 import os
 
-test_split = 0.3
-
-file1 = open(pwd+'testlist.txt','w')
-file2 = open(pwd+'trainlist.txt','w')
+file1 = open(pwd+'datalist.txt','w')
 
 names = os.listdir(pwd+'../Peliculas/noFights')
-k = int(len(names)*test_split)
-for name in names[:k]:
+for name in names:
 	file1.write('noFights/'+name+' 1\n')
-for name in names[k:]:
-	file2.write('noFights/'+name+' 1\n')
 
 names = os.listdir(pwd+'../Peliculas/fights')
-k = int(len(names)*test_split)
-for name in names[:k]:
+for name in names:
 	file1.write('fights/'+name+' 2\n')
-for name in names[k:]:
-	file2.write('fights/'+name+' 2\n')
 
 file1.close()
-file2.close()
 
 with open(pwd+'classInd.txt','w') as file:
 	file.write('1 NonViolence\n2 Violence')

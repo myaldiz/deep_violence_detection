47530cf387001ee9b5a9f062675229377b53221a
Mustafa Berk Yaldiz <berk17@gmail.com>
2019-04-25 13:13:04 -0400

    Update default_settings.py

--- ViolanceDetection-Python/default_settings.py
+++ ViolanceDetection-Python/default_settings.py
@@ -8,7 +8,7 @@ model_settings = {
     'learning_rate': 1e-4,  # 1e-4 from previous code
     'lr_decay': 0.5,
     'decay_epoch': 3,
-    'weight_decay': 0.001,
+    'weight_decay': 0.0007,
     'dropout': 0.5,
     'summary_checkpoints': 50,  # Number of steps to create checkpoint
     'model_save_checkpoints': 300,
@@ -39,8 +39,8 @@ model_settings = {
     'model_name': 'UCF_finetune',
     'checkpoint_dir': './checkpoints/',
     'model_save_dir': './models/',
-    # 'model_read_loc': './models/s1m-ucf101.model',
-    'model_read_loc': './models/2019-04-25__16-10-31/UCF_finetune-1592',
+    'model_read_loc': './models/s1m-ucf101.model',
+    # 'model_read_loc': './models/2019-04-25__16-10-31/UCF_finetune-1592',
     # 'model_read_loc': './models/UCF_finetuneFC_last.model',
     'data_home': '../datasets/UCF-101-Frames/',
     'train_test_loc': '../datasets/UCF-ActionRecognitionSplits',

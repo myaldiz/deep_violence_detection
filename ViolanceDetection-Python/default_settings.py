825dfd4e92b06e44713e22b6755ebe36cad77a80
Mustafa Berk Yaldiz <berk17@gmail.com>
2019-04-30 14:50:35 -0400

    Update default_settings.py

--- ViolanceDetection-Python/default_settings.py
+++ ViolanceDetection-Python/default_settings.py
@@ -44,9 +44,9 @@ model_settings = {
     # 'model_read_loc': './models/UCF_finetuneFC_last.model',
     'data_home': '../datasets/UCF-101-Frames/',
     'train_test_loc': '../datasets/UCF-ActionRecognitionSplits',
-    'train_file_name': '/testlist01.txt',
+    'train_file_name': '/trainlist01.txt',
     # 'train_file_name': '/train_small.txt',
-    'test_file_name': '/testlist01.txt',
+    'test_file_name': '/trainlist01.txt',
     'mean_clip_loc': '../datasets/PreprocessData/crop_mean.npy',
 
     # Feature Extraction Directory Settings
